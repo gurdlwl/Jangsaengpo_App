57b18c4271d1a660d7889e4b0337dce424117c38
gurdlwl <gurdlwl@gmail.com>
2019-05-08 20:08:49 +0900

    [ADD]

---
+++ app/src/main/java/kr/hs/dgsw/jangsengpoapp/StorytellingActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.jangsengpoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StorytellingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storytelling);
+    }
+}
