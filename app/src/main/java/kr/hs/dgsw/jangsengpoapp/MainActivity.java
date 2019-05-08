c04b110819d52afe221b48401d8fd58c150ba480
gurdlwl <gurdlwl@gmail.com>
2019-05-08 20:09:22 +0900

    [UPDATE]

--- app/src/main/java/kr/hs/dgsw/jangsengpoapp/MainActivity.java
+++ app/src/main/java/kr/hs/dgsw/jangsengpoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.dgsw.jangsengpoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
