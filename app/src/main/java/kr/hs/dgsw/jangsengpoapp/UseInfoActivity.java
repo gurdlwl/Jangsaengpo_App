05cb03582ddd97a08b4d1aa50dab02e986db3f12
gurdlwl <gurdlwl@gmail.com>
2019-06-17 18:58:58 +0900

    fee_info text underline 추가

--- app/src/main/java/kr/hs/dgsw/jangsengpoapp/UseInfoActivity.java
+++ app/src/main/java/kr/hs/dgsw/jangsengpoapp/UseInfoActivity.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.View;
+import android.widget.TextView;
 
 public class UseInfoActivity extends AppCompatActivity {
 
     final String url = "http://www.whalecity.kr/EgovPageLink.do?link=city/info/fee";
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_use_info);
+
+        SpannableString text = new SpannableString(getString(R.string.fee_info));
+        text.setSpan(new UnderlineSpan(), 0, text.length(), 0);
+        tv.setText(text);
     }
 
     public void onFeeTextClick(View v){
