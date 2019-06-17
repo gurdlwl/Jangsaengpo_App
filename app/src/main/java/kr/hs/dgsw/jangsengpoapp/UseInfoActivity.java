ffd26ed20dcf524c72c37996b8cb6aae8294c245
gurdlwl <gurdlwl@gmail.com>
2019-06-17 19:28:27 +0900

    fee_info text underline 제거 및 > 추가

--- app/src/main/java/kr/hs/dgsw/jangsengpoapp/UseInfoActivity.java
+++ app/src/main/java/kr/hs/dgsw/jangsengpoapp/UseInfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.view.View;
@@ -18,10 +19,6 @@ public class UseInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_use_info);
-
-        SpannableString text = new SpannableString(getString(R.string.fee_info));
-        text.setSpan(new UnderlineSpan(), 0, text.length(), 0);
-        tv.setText(text);
     }
 
     public void onFeeTextClick(View v){
